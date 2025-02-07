0f639f151d31883c50604886f1847c824b1e0ec8
hitalin <hitalin@proton.me>
2025-02-07 14:27:19 +0900

    fix: Restore backup.sh to be able to read environment variables on its own

--- src/backup.sh
+++ src/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# 環境変数設定スクリプトの追加
-COPY ./src/env.sh /opt/misskey-backup/env.sh
-RUN chmod +x /opt/misskey-backup/env.sh
+# 環境変数の読み込み
+if [ -f /config/.env ]; then
+    source /config/.env
+fi
 
 # バックアップディレクトリの作成（日付別）
 BACKUP_DATE=$(TZ='Asia/Tokyo' date +%Y-%m-%d)
